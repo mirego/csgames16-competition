660c829e25e430f3c8d0dc07e16dbdc043c2e33f
Émile Bélair <ebelair@me.com>
2016-03-03 01:33:35 -0500

    Add messages

--- server/public/javascripts/global.js
+++ server/public/javascripts/global.js
@@ -4,8 +4,8 @@ var userListData = [];
 // DOM Ready =============================================================
 $(document).ready(function() {
 
-  // Populate the user table on initial page load
-  populateTable();
+  // Populate the tables on initial page load
+  populateTables();
 
   // Username link click
   $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
@@ -21,12 +21,13 @@ $(document).ready(function() {
 // Functions =============================================================
 
 // Fill table with data
-function populateTable() {
+function populateTables() {
 
   // Empty content string
-  var tableContent = '';
+  var userTableContent = '';
+  var messageTableContent = '';
 
-  // jQuery AJAX call for JSON
+  // jQuery AJAX call for users JSON
   $.getJSON('/users', function(data) {
 
     // Stick our user data array into a userlist variable in the global object
@@ -34,15 +35,35 @@ function populateTable() {
 
     // For each item in our JSON, add a table row and cells to the content string
     $.each(data, function() {
-      tableContent += '<tr>';
-      tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.name + '" title="Show Details">' + this.name + '</a></td>';
-      tableContent += '<td>' + this.email + '</td>';
-      tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
-      tableContent += '</tr>';
+      userTableContent += '<tr>';
+      userTableContent += '<td><a href="#" class="linkshowuser" rel="' + this.name + '" title="Show Details">' + this.name + '</a></td>';
+      userTableContent += '<td>' + this.email + '</td>';
+      userTableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+      userTableContent += '</tr>';
     });
 
     // Inject the whole content string into our existing HTML table
-    $('#userList table tbody').html(tableContent);
+    $('#userList table tbody').html(userTableContent);
+  });
+  
+  // jQuery AJAX call for messages JSON
+  $.getJSON('/messages', function(data) {
+
+    // For each item in our JSON, add a table row and cells to the content string
+    $.each(data, function() {
+      var searchId = this.userId;
+      var userName = "";
+      $.each(userListData, function() {
+        if (this["_id"] == searchId) userName = this.name
+      });
+      messageTableContent += '<tr>';
+      messageTableContent += '<td>' + userName + '</td>';
+      messageTableContent += '<td><img src="data:image/png;base64,' + this.message + '" height="120"></td>';
+      messageTableContent += '</tr>';
+    });
+
+    // Inject the whole content string into our existing HTML table
+    $('#messageList table tbody').html(messageTableContent);
   });
 };
 
